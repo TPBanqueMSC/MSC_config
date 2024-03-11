d9ac76b362cfafe7ae774ffe29c4af2fa069d945
Yohan <=yohan.lussiez@outlook.fr>
2024-03-11 01:08:36 +0100

    Changed file

--- src/main/java/org/example/config/ConfigApplication.java
+++ src/main/java/org/example/config/ConfigApplication.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
